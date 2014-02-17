b54ed2ae2fd099f05d245505108b17a9732a16b7
mmilicevic <m.milicevic@onehippo.com>
2014-02-17 17:25:17 +0100

    - add popup - add check for catalina.sh in the parent dir

--- src/com/machak/idea/plugins/tomcat/actions/DeleteTomcatWebapps.java
+++ src/com/machak/idea/plugins/tomcat/actions/DeleteTomcatWebapps.java
@@ -6,10 +6,15 @@
 
 package com.machak.idea.plugins.tomcat.actions;
 
+import java.awt.event.ActionEvent;
 import java.io.File;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import com.google.common.base.Strings;
-import com.google.common.io.Files;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
@@ -17,9 +22,10 @@ import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogBuilder;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.io.FileUtil;
 import com.machak.idea.plugins.tomcat.config.ApplicationComponent;
 import com.machak.idea.plugins.tomcat.config.ProjectComponent;
@@ -62,23 +68,84 @@ public class DeleteTomcatWebapps extends AnAction {
             if (!webappsDirectory.endsWith(File.separator)) {
                 webappsDirectory = webappsDirectory + File.separator;
             }
-            final File sharedDirectory = new File(webappsDirectory);
-            if (!sharedDirectory.exists()) {
+            final File webappDir = new File(webappsDirectory);
+            if (!webappDir.exists()) {
                 error("Tomcat webapps directory does not exists: " + webappsDirectory);
                 return;
             }
+            // check if tomcat directory:
+            final File tomcatRoot = webappDir.getParentFile();
 
-
-            final String[] filePaths = sharedDirectory.list();
-            //if (!component.isShowDialog()) {
-            for (String name : filePaths) {
-                deleteFile(new File(String.format("%s%s", webappsDirectory, name)));
+            final File[] files = tomcatRoot.listFiles();
+            if (files == null) {
+                return;
             }
-            //}
+            boolean invalidDir = true;
+            mainLoop:
+            for (File file : files) {
+                if (file.getName().equals("bin")) {
+                    final File[] configFiles = file.listFiles();
+                    if (configFiles != null) {
+                        for (File configFile : configFiles) {
+                            final String name = configFile.getName();
+                            if (name.equals("catalina.sh") || name.equals("catalina.bat")) {
+                                invalidDir = false;
+                                break mainLoop;
+                            }
+                        }
+                    }
+                }
+            }
+            if (invalidDir) {
+                error("Tomcat webapps directory is not a tomcat directory: " + webappsDirectory);
+                return;
+            }
+
+            if (component.isShowDialog()) {
+                showDeletePopup(webappsDirectory, webappDir);
+
+            } else {
+                deleteFiles(webappsDirectory, webappDir);
+            }
 
         }
 
     }
+
+    private void deleteFiles(final String webappsDirectory, final File webappDir) {
+        final String[] filePaths = webappDir.list();
+        for (String name : filePaths) {
+            deleteFile(new File(String.format("%s%s", webappsDirectory, name)));
+        }
+    }
+
+    private void showDeletePopup(final String webappsDirectory, final File webappDir) {
+
+        final String absolutePath = webappDir.getAbsolutePath();
+        final DialogBuilder dialogBuilder = new DialogBuilder(project);
+        dialogBuilder.setTitle("Create project file:");
+        final JPanel simplePanel = new JPanel();
+        simplePanel.add(new JLabel("All directories within: " + absolutePath + " will be deleted"));
+        dialogBuilder.setCenterPanel(simplePanel);
+
+        final Action acceptAction = new AbstractAction() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                deleteFiles(webappsDirectory, webappDir);
+                dialogBuilder.getDialogWrapper().close(DialogWrapper.OK_EXIT_CODE);
+
+
+            }
+        };
+        acceptAction.putValue(Action.NAME, "OK");
+        dialogBuilder.addAction(acceptAction);
+
+        dialogBuilder.addCancelAction();
+        dialogBuilder.showModal(true);
+    }
+
 
     private void deleteFile(final File file) {
         final String filePath = file.getPath();
